9671b900876d5fc8473905d60e6d46230e6339ef
Zer0Capitalist <chrisp2910@osloskolen.no>
2022-05-03 10:03:07 +0200

    idk

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 var klikkeKnappEl = document.getElementById("klikkeknapp");
+var utskriftEl = document.getElementById("utskrift");
+var oppgradering1El = document.getElementById("oppgradering1");
+var oppgradering2El = document.getElementById("oppgradering2");
 
 klikkeKnappEl.addEventListener("click", beregnKlikk)
+oppgradering1El.addEventListener("click", oekpoengperklikk)
+
+var antallKlikk = 0;
+var poengperklikk = 1;
 
 function beregnKlikk(e) {
-    console.log("hei");
+    antallKlikk = antallKlikk + poengperklikk;
+    utskriftEl.innerHTML = "Du har klikket " + antallKlikk + " ganger.";
+    
 }
 
+function oekpoengperklikk(e){
+    if(antallKlikk > 99){
+        antallKlikk = antallKlikk - 100;
+        poengperklikk++;
+        klikkeKnappEl.innerHTML = "+" + poengperklikk;
+        utskriftEl.innerHTML = "Du har kjøpt en oppgradering og har nå " + antallKlikk + " poeng igjen";
+    } else {
+        alert("Du har ikke nok poeng enda, klikk mer.")
+    }
+   
+    }
