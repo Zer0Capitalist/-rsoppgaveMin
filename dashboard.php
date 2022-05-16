49c1f1cd739221e965e514174446ef14c3af6337
Zer0Capitalist <chrisp2910@osloskolen.no>
2022-05-16 11:40:17 +0200

    added more of everything

--- dashboard.php
+++ dashboard.php
@@ -14,6 +14,7 @@ include("auth_session.php");
         <p>Hey, <?php echo $_SESSION['username']; ?>!</p>
         <p>You are now user dashboard page.</p>
         <p><a href="logout.php">Logout</a></p>
+        <p><a href="contact.php">contact us</a></p>
     </div>
 </body>
 </html>
